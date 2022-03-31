3937d32b5ea952c9d7738ba83462c71d374e5f69
Hieu Tran <hieu.tran@perficient.com>
2022-03-31 16:00:13 -0700

    almost done with Section 4

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
-import Expenses from "./components/Expenses";
-import bulba from './bulba.PNG';
-import'./App.css'
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import "./App.css";
 
 function App() {
-
   const expenses = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
+      id: "e1",
+      title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
   ];
 
+  function addExpenseHandler(expense) {
+    console.log("Detected in App.js");
+    console.log(expense);
+  }
   return (
-    <div className='WholePage'>
-      <h2>Bulbasaur best boi.</h2>
-      <h2>Charmander suxs to squirt squirt.</h2>
-      <img src = {bulba} alt='best boi' width = '325' height = '400'></img>
+    <div className="WholePage">
+      {/* in NewExpense, we're passing the addExpenseHandler 
+      function from App.js to NewExpense.js, where NewExpense.js 
+      will take an object and stuff it back into the function that was passed */}
+      <NewExpense onAddExpense={addExpenseHandler} />
       {/*can use components as custom HTML elements*/}
-      <Expenses items = {expenses}/>
+      {/* Expenses passes the expenses list from App.js to Expenses.js to use */}
+      <Expenses items={expenses} />
     </div>
   );
 }
